5e37bf1b2290124b4bc32660cbb8b63611e59492
sadoyui <wiliamweiyi@gmail.com>
2023-08-22 00:15:59 +0000

    mars: Miku-Ui ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_mars.mk
-
-COMMON_LUNCH_CHOICES := \
-    arrow_mars-user \
-    arrow_mars-userdebug \
-    arrow_mars-eng
+    $(LOCAL_DIR)/miku_mars.mk
--- miku_mars.mk
+++ miku_mars.mk
@@ -11,24 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mars device
 $(call inherit-product, device/xiaomi/mars/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Miku stuff
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mars
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2102K1AC
-PRODUCT_NAME := arrow_mars
-
-ARROW_GAPPS := true
+PRODUCT_NAME := miku_mars
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_SYSTEM_NAME := mars_global
 PRODUCT_SYSTEM_DEVICE := mars
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mars_global-user 13 RKQ1.211001.001 V14.0.2.0.TKAMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-BUILD_FINGERPRINT := Xiaomi/mars_global/mars:13/RKQ1.211001.001/V14.0.2.0.TKAMIXM:user/release-keys
+# Maintainer
+MIKU_MASTER := William_Sado
